d64b905a966f1ab3b5a7da9f642ba5232754994c
m00tt <andreamottini15@gmail.com>
2021-05-22 20:45:55 +0200

    Improved control functions

--- src/utils/GeneralFunctions.java
+++ src/utils/GeneralFunctions.java
@@ -14,6 +14,8 @@ import java.nio.file.Files;
 import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
 import java.util.List;
 import static javax.swing.JOptionPane.showMessageDialog;
+import java.util.regex.Matcher; 
+import java.util.regex.Pattern;
 
 
 
@@ -22,6 +24,9 @@ public class GeneralFunctions {
     public static String WORKINGDIR = System.getProperty("user.dir") + File.separator + "data";
     public static String CENTRIVACCINALIDIR = WORKINGDIR + File.separator + "centri_vaccinali";
     public static String CITTADINIDIR = WORKINGDIR + File.separator + "cittadini";
+    
+    static Pattern onlyLettersPattern = Pattern.compile("[^a-zA-Z]");
+    static Pattern onlyNumbersPattern = Pattern.compile("[^0-9]");
     
     public static boolean checkDirHierarchy(){
         boolean check = true;
@@ -73,32 +78,22 @@ public class GeneralFunctions {
  
     //Controlli per campi di inserimento
     public static boolean checkProvincia(String p){
-        //controllare se sono solamente caratteri
-        return p.length() == 2;
+        Matcher matcher = onlyLettersPattern.matcher(p);
+        return p.trim().length() == 2 && !matcher.find();
     }
     
     public static boolean checkCAP(String p){
-        
-        if(p.length()!=5){
-            return false;
-        }
-        else{
-            try{
-                int parse = Integer.parseInt(p);
-                return true;
-            }
-            catch(NumberFormatException e){
-                return false;
-            }
-        }
+        Matcher matcher = onlyNumbersPattern.matcher(p);
+        return p.trim().length() == 5 && !matcher.find();
     }
     
     public static boolean checkCompiled(String p){
-        return !(p.length()<4 || p.contains("-"));
+        Matcher matcher = onlyLettersPattern.matcher(p);
+        return p.trim().length()>3 && !matcher.find();
     }
     
     public static boolean checkCivico(String p){
-        return p.length()>0;
+        return p.trim().length()>0;
     }
     
     
